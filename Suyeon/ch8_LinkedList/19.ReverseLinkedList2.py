49c19a2d2d803472954319772163dd73c8ca1cb4
suy2on <tnwjdrms@naver.com>
2021-08-12 17:40:42 +0900

    19번문제풀이추가

--- Suyeon/ch8_LinkedList/19.ReverseLinkedList2.py
+++ Suyeon/ch8_LinkedList/19.ReverseLinkedList2.py
@@ -39,14 +39,47 @@ class Solution:
 
 
 
+class Solution:
+    def reverseBetween(self, head,  left, right):
+
+        root = start = ListNode(None)
+        root.next = head
+
+        for _ in range(left-1):
+            start = start.next
+
+        end = start.next
+
+        for _ in range(right - left):
+            start.next, end.next.next, end.next = end.next, start.next, end.next.next
+
+        return root.next
 
 
 
 
+        else:
+            link1 = head    #link1은 head에서 시작
+            for _ in range(left-2):  # 반전할 지점 바로 전까지 link1옮겨주기
+                link1 = link1.next
+            p = link1.next   # 다 옮겼다면 p는 left값
+
+        #link2, q설정
+        link2 = p   # link2 저장
+        q= p.next   # q는 p바로다음
 
 
-# range, 슬라이싱 문법 차이
+        # q하나씩 가져와서 p앞에 붙이고 p는 rev의 헤드유지
+        for _ in range(right - left):
+            p, q.next , q = q, p, q.next
+
+        #이어주기
+        if link1:  #link1이 있다면 이어주기
+            link1.next = p
+        if not link1: #link1없다면 head를 p로
+            head = p
+
+        link2.next = q   # link2이어주기
 
 
-
-print(list(range(2,-1,-1)))
+        return head
