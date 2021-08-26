ace20b53c2085f13716a3dc9e8f3acf0817f3d20
suy2on <tnwjdrms@naver.com>
2021-08-26 15:20:19 +0900

    #39 solve: recursive dfs

--- Suyeon/ch12_Graph/39_Combination_Sum.py
+++ Suyeon/ch12_Graph/39_Combination_Sum.py
@@ -1,33 +1,25 @@
 def combinationSum(candidates, target):
     result = []
     pre_list = []
-    back = 0
+
 
 
     def dfs(start):
-        global back
         if sum(pre_list) > target:
-            back = 1
             return
         if sum(pre_list) == target:
             result.append(pre_list[:])
-            back = 1
             return
-        for num in candidates[start:]:
-            pre_list.append(num)
-            dfs(start)
-            if pre_list:
-                pre_list.pop()
-            if back:
-                back = 0
-                break
+        for i in range(start,len(candidates)):
+            pre_list.append(candidates[i])
+            dfs(i)
+            pre_list.pop()
 
-    for i in range(len(candidates)):
-        pre_list.clear()
-        pre_list.append(candidates[i])
-        dfs(i)
+    dfs(0)
+
 
     return result
+
 
 
 
