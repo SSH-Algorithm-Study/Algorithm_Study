a33a23a5ab9cbfb467442c1c24f3c9b15de3f44f
suy2on <tnwjdrms@naver.com>
2021-09-13 23:13:30 +0900

    # 787 solve : time error

--- Suyeon/ch13_ShortRoute/787_Cheapest_Flights_within_K_stops.py
+++ Suyeon/ch13_ShortRoute/787_Cheapest_Flights_within_K_stops.py
@@ -2,32 +2,24 @@ import heapq
 import collections
 
 def findCheapestPrice( n: int, flights, src: int, dst: int, k: int) -> int:
-    dist = collections.defaultdict(tuple)  # 시작점부터 노드까지의 최단거리
+    result = collections.defaultdict()  # 시작점부터 노드까지의 최단거리
     pairs = [(0, src, -1)]  # 최소 우선순위 큐
     info = collections.defaultdict(list) # 초기연결정보
 
     for flight in flights:
         info[flight[0]].append((flight[1], flight[2]))
 
+
     while pairs:
-        print(dist)
-        print(pairs)
-        cost, node, mid = heapq.heappop(pairs)
-        if mid < k and dist
-        if node not in dist or (node in dist and dist[node][1]) > k:  # 없다면 넣어주기
-            dist[node] = (cost, mid)
-            for n, c in info[node]: # 이웃노드들 정보가져오기
-                heapq.heappush(pairs, (c+cost, n, mid+1 ))
+        price, node, middle = heapq.heappop(pairs)
+        if middle > k:  # 경유지가 k가 넘으면 더 이상 필요없는 경로
+            continue
+        if not result[node]:  # 이제까지 k이내의 최소 경로가 없었다면 넣어주기
+            result[node] = price
+        for n, p in info[node]: # 이웃경로정보넣어주기
+            heapq.heappush(pairs, (p+price, n, middle+1))
 
-
-    if dist[dst] and dist[dst][1] <= k :
-        return dist[dst][0]
+    if result[dst]:
+        return result[dst]
     else:
         return -1
-
-
-print(findCheapestPrice(4,
-[[0,1,1],[0,2,5],[1,2,1],[2,3,1]],
-0,
-3,
-1))
