37ab20433b77efeb5b2b53fa6a8d484586419df0
suy2on <tnwjdrms@naver.com>
2021-09-15 00:57:40 +0900

    # 617 solve : recursion dfs

---
+++ Suyeon/ch14_Tree/617_Merge_Two_Binary_Trees.py
@@ -0,0 +1,29 @@
+class Solution:
+    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        state = 0
+        def dfs(root1, root2):
+            nonlocal state
+            if root1 is None and root2 is None: # 둘다 null일경우
+                return
+            if root1 is None: # root1만 null일 경우
+                state = 1
+                return
+            if root2 is None: # root2만 null일 경우
+                return
+            root1.val = root2.val + root1.val # root1, root2 둘다 null이 아닐경우
+
+            dfs(root1.left, root2.left)
+            if state == 1:
+                root1.left = root2.left
+                state = 0
+            dfs(root1.right, root2.right)
+            if state == 1:
+                root1.right = root2.right
+                state = 0
+
+
+        dfs(root1, root2)
+        if state == 1:  # root1이 빈값으로 들어가면
+            return root2
+        else:
+            return root1
