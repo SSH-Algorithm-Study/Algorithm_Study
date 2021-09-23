38994fa7f17aebfca8d48933f127cc177b83432d
suy2on <tnwjdrms@naver.com>
2021-09-24 01:31:18 +0900

    # 310 solve: loops

--- Suyeon/ch14_Tree/310_Minimum_Height_Trees.py
+++ Suyeon/ch14_Tree/310_Minimum_Height_Trees.py
@@ -10,7 +10,7 @@ import sys
 #         info[edge[1]].append(edge[0])
 #
 #
-#     for i in range(n):
+#     for i in range(n): # 모든 노드를 한번씩 노드로
 #         discovered = []
 #         queue = collections.deque()
 #         queue.append(i)
@@ -35,46 +35,76 @@ import sys
 #     result = sorted(result.items(), key=operator.itemgetter(0)) #작은 값들 node출력  ( Key기준으로 정렬 )
 #
 #     return result[0]
+#
+#
+#
+#
+#
+#
+#
+# def findMinHeightTrees( n, edges):
+#     info = collections.defaultdict(list)
+#     result = collections.defaultdict(list)
+#     min_height = sys.maxsize
+#
+#     for edge in edges:  # 그래프 연결관계 기록
+#         info[edge[0]].append(edge[1])
+#         info[edge[1]].append(edge[0])
+#
+#
+#     for i in range(n):
+#         discovered = []
+#         queue = collections.deque()
+#         queue.append(i)
+#         height = -1
+#         while queue:
+#             if height > min_height:
+#                 break
+#             loop = len(queue) #부모만큼 돌기
+#             height += 1
+#             for _ in range(loop):
+#                 node = queue.popleft()
+#                 discovered.append(node)
+#                 for child in info[node]:
+#                     if child not in discovered:
+#                         queue.append(child)
+#
+#         result[height].append(i)
+#         min_height = min(min_height, height)
+#
+#     return result[min_height]
 
 
+class Solution:
+    def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+        if n == 1:
+            return [0]
+
+        info = collections.defaultdict(list)
+
+        for edge in edges:  # 그래프 연결관계 기록
+            info[edge[0]].append(edge[1])
+            info[edge[1]].append(edge[0])
+
+        leaves = []
+        for key in info:
+            if len(info[key]) == 1:
+                leaves.append(key)
+
+        while n > 2:
+            n -= len(leaves)  # 남은 노드 수
+            new_leave = []
+            for leaf in leaves:
+                node = info[leaf].pop()
+                info[node].remove(leaf)
+
+                if len(info[node]) == 1:
+                    new_leave.append(node)
+
+            leaves = new_leave
+
+        return leaves
 
 
-
-
-
-def findMinHeightTrees( n, edges):
-    info = collections.defaultdict(list)
-    result = collections.defaultdict(list)
-    min_height = sys.maxsize
-
-    for edge in edges:  # 그래프 연결관계 기록
-        info[edge[0]].append(edge[1])
-        info[edge[1]].append(edge[0])
-
-
-    for i in range(n):
-        discovered = []
-        queue = collections.deque()
-        queue.append(i)
-        height = -1
-        while queue:
-            if height > min_height:
-                break
-            loop = len(queue) #부모만큼 돌기
-            height += 1
-            for _ in range(loop):
-                node = queue.popleft()
-                discovered.append(node)
-                for child in info[node]:
-                    if child not in discovered:
-                        queue.append(child)
-
-        result[height].append(i)
-        min_height = min(min_height, height)
-
-    return result[min_height]
-
-
-print(findMinHeightTrees( 6, [[0,1],[0,2],[0,3],[3,4],[4,5]]))
 
 
