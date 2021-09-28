46d57f389d984a402783540d9e5938f71f5020a6
suy2on <tnwjdrms@naver.com>
2021-09-28 11:49:44 +0900

    # 938 solve : 함수정리

--- Suyeon/ch14_Tree/938_Range_Sum_of_BST.py
+++ Suyeon/ch14_Tree/938_Range_Sum_of_BST.py
@@ -1,4 +1,4 @@
-Definition for a binary tree node.
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -38,25 +38,29 @@ class Solution:
         return sum
 
 
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        sum = 0
 
-def dfs(root):
+        def dfs(root):
 
-    if root is None:
-        return 0
+            if root is None:
+                return 0
 
-    elif root.val == low:
-        return root.val + dfs(root.right)
+            elif root.val == low:
+                return root.val + dfs(root.right)
 
-    elif root.val == high:
-        return root.val + dfs(root.left)
+            elif root.val == high:
+                return root.val + dfs(root.left)
 
-    elif root.val > low and root.val < high:  # 사이일 때
-        return root.val + dfs(root.left) + dfs(root.right)
+            elif root.val > low and root.val < high:  # 사이일 때
+                return root.val + dfs(root.left) + dfs(root.right)
 
-    elif root.val < low:  # low 보다 작을 때
-        return dfs(root.right)
+            elif root.val < low:  # low 보다 작을 때
+                return dfs(root.right)
 
-    else:  # height 보다 클 때
-        return dfs(root.left)
+            else:  # height 보다 클 때
+                return dfs(root.left)
 
 
+        return dfs(root)
