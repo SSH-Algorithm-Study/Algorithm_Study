6e7bb97b3308ee8a6466d64892cfa7909961f3ad
suy2on <tnwjdrms@naver.com>
2021-11-04 22:59:25 +0900

    # feat: 주석추가

--- Suyeon/ch23_DP/70_Climing_Stairs.py
+++ Suyeon/ch23_DP/70_Climing_Stairs.py
@@ -4,6 +4,6 @@ class Solution:
 
         if n > 2:
             for i in range(2, n):
-                dp.append(dp[i - 1] + dp[i - 2])
+                dp.append(dp[i - 1] + dp[i - 2]) # 마지막이 2칸일때와 1칸일때의 경우 더하기
 
         return dp[n - 1]
