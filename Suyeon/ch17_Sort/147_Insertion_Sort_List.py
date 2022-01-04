5af5058f57895888e07665ddc6819d719a8d2fa1
suy2on <tnwjdrms@naver.com>
2022-01-04 14:55:25 +0900

    # 147 최적화

--- Suyeon/ch17_Sort/147_Insertion_Sort_List.py
+++ Suyeon/ch17_Sort/147_Insertion_Sort_List.py
@@ -27,3 +27,36 @@ class Solution:
 
         return slist.next
 
+# 한줄로 쓰기
+# p.next가 끝나던 p.next.val >= head.val이여서 자리를 찾던 넣어줘야한다 -> 합치기
+class Solution:
+    def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        p = slist = ListNode()
+
+        while head:
+            while p.next and p.next.val < head.val: # p값이 head값보다 작으면
+                p = p.next
+
+            p.next, head.next, head = head, p.next, head.next   # 중간에 삽입
+
+            p = slist
+
+        return slist.next
+
+#비교개선
+class Solution:
+    def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        p = slist = ListNode()
+
+        while head:
+            while p.next and p.next.val < head.val:  # p값이 head값보다 작으면
+                p = p.next
+
+            p.next, head.next, head = head, p.next, head.next
+
+            if head and p.next.val > head.val: # 정렬한것중 최대값보다 작다면
+                p = slist # 처음부터 비교
+
+        return slist.next
+
+
