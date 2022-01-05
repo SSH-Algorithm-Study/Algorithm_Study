312129c0dc60715f85b086bd0e5a3d84d8b2c8a6
suy2on <tnwjdrms@naver.com>
2022-01-05 19:41:40 +0900

    # 973 solve : heapq

--- Suyeon/ch17_Sort/973_K_Closest_Points_to_Origin.py
+++ Suyeon/ch17_Sort/973_K_Closest_Points_to_Origin.py
@@ -5,4 +5,15 @@ class Solution:
         return points[:k]
 
 
+import heapq
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        pq = []
+        result = []
+        for i,point in enumerate(points):
+            heapq.heappush(pq, (point[0]**2 + point[1]**2, i))
 
+        for i in range(k):
+            result.append(points[heapq.heappop(pq)[1]])
+
+        return result
