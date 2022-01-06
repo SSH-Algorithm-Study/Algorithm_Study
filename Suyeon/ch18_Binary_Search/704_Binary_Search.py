461efea45d20a4a759a578b58c17d57e8f6642f0
suy2on <tnwjdrms@naver.com>
2022-01-06 14:39:39 +0900

    # 18 solve : loops

--- Suyeon/ch18_Binary_Search/704_Binary_Search.py
+++ Suyeon/ch18_Binary_Search/704_Binary_Search.py
@@ -5,7 +5,7 @@ class Solution:
             if left > right:
                 return -1
 
-            mid = (left + right) // 2
+            mid = left + (right - left) // 2
             if nums[mid] == target :
                 return mid
             elif nums[mid] > target :
@@ -14,3 +14,18 @@ class Solution:
                 return BS(mid+1, right)
 
         return BS(0, len(nums)-1)
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums)-1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target :
+                return mid
+            elif nums[mid] > target :
+                right = mid -1
+            else:
+                left = mid + 1
+
+        return -1
