db4b876db99bc4f1aa55075405ab1de643ccfd91
suy2on <tnwjdrms@naver.com>
2022-01-08 21:50:50 +0900

    # 349 solve : defaultdict

--- Suyeon/ch18_Binary_Search/349_Intersection_of_two_Arrays.py
+++ Suyeon/ch18_Binary_Search/349_Intersection_of_two_Arrays.py
@@ -6,3 +6,21 @@ class Solution:
         result = [ num  for num in nums1 if num in nums2 ]
 
         return result
+
+
+import collections
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        dict1 = collections.defaultdict(int)
+
+        for num in nums1:
+            dict1[num] += 1
+
+        result = []
+
+        for num in nums2:
+            if dict1[num] != 0:
+                result.append(num)
+
+        return result
