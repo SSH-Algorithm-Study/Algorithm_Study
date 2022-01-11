c866878f085bb227677dded07c88ebff82dced90
suy2on <tnwjdrms@naver.com>
2022-01-11 16:26:17 +0900

    # 33 solve : BS 새로운 기준

--- Suyeon/ch18_Binary_Search/33_Search_in_Rotated_Sorted_Array.py
+++ Suyeon/ch18_Binary_Search/33_Search_in_Rotated_Sorted_Array.py
@@ -27,3 +27,29 @@ class Solution:
                 right = mid - 1
 
         return -1
+
+# 이진탐색 새로운 기준
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left , right = 0 , len(nums)-1
+
+
+        while left <= right:
+            mid = left + (right - left) // 2
+
+            if nums[mid] == target:
+                return mid
+
+            if nums[mid] <= nums[right]: # pivot이 왼쪽에 존재할 경우
+                if nums[mid] <= target and target <= nums[right]: # target이 mid와 right의 사이값이면 그 사이에 존재
+                    left = mid + 1
+                else: # 아니라면 왼쪽편에 target 존재
+                    right = mid - 1
+
+            if nums[mid] >= nums[left]: # pivot이 오른쪽에 존재할 경우
+                if nums[mid] >= target and nums[left] <= target: # target이 mid와 left의 사이값이면 그 사이에 존재
+                    right = mid - 1
+                else: # 아니라면 오른편에 target 존재
+                    left = mid + 1
+
+        return -1
