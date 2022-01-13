788f1df25b1bbf2ff6616a7299d3924d9ead71fb
suy2on <tnwjdrms@naver.com>
2022-01-13 21:03:23 +0900

    # 33 solve : %연산, pivot값으로 보정

--- Suyeon/ch18_Binary_Search/33_Search_in_Rotated_Sorted_Array.py
+++ Suyeon/ch18_Binary_Search/33_Search_in_Rotated_Sorted_Array.py
@@ -53,3 +53,34 @@ class Solution:
                     left = mid + 1
 
         return -1
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums)-1
+
+        # 최소값 인덱스 찾기
+        while left < right:
+            mid = left + (right - left) // 2
+
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            else:
+                right = mid
+
+        pivot = left
+
+        # target 찾기
+        left, right = 0, len(nums)-1
+
+        while left <= right:
+            mid = left + (right-left)//2
+            mid_pivot = (mid+pivot) % len(nums)
+
+            if nums[mid_pivot] < target:
+                left = mid + 1
+            elif nums[mid_pivot] > target:
+                right = mid - 1
+            else:
+                return mid_pivot
+
+        return -1
