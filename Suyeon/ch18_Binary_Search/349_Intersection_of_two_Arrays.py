3daf41d5d461de35417424a7f4bd92cbaa0aebba
suy2on <tnwjdrms@naver.com>
2022-01-13 21:02:55 +0900

    # 349 solve : BS 2번

--- Suyeon/ch18_Binary_Search/349_Intersection_of_two_Arrays.py
+++ Suyeon/ch18_Binary_Search/349_Intersection_of_two_Arrays.py
@@ -24,3 +24,46 @@ class Solution:
                 result.append(num)
 
         return result
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        result = set()
+
+        nums2.sort()
+
+        for num in nums1:
+            first, last = 0, len(nums2)-1
+
+            while first <= last:
+                mid = first+(last-first)//2
+
+                if nums2[mid] == num:
+                    result.add(num)
+                elif nums2[mid] > num:
+                    last = mid - 1
+                else:
+                    first = mid + 1
+
+        return result
+
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        result = set()
+
+        nums1.sort()
+        nums2.sort()
+
+        i=j=0
+
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] > nums2[j]:
+                j += 1
+            elif nums1[i] < nums2[j]:
+                i += 1
+            else:
+                result.add(nums1[i])
+                i += 1
+                j += 1
+
+        return result
