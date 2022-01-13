3318a7385f7f7706058b2d4e0f84107ad7533027
suy2on <tnwjdrms@naver.com>
2022-01-13 21:02:21 +0900

    # 167 solve : BS

--- Suyeon/ch18_Binary_Search/167_Two_Sum_II.py
+++ Suyeon/ch18_Binary_Search/167_Two_Sum_II.py
@@ -12,3 +12,20 @@ class Solution:
 
             else:
                 return [i+1,j+1]
+
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        for k, v in enumerate(numbers):
+            left, right = k+1, len(numbers)-1 # k보다 오른쪽에서 탐색 앞쪽은 이미 탐색했으므로 
+            target = target - v
+
+            while left <= right:
+                mid = left + (right-left)//2
+
+                if numbers[mid] < target :
+                    left = mid + 1
+                elif numbers[mid] > target:
+                    right = mid -1
+                else:
+                    return k+1, mid+1
