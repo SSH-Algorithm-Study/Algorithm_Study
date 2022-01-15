9b1d4b88121984f8cbc748fa8bb7f32361f41b6a
ssssujini99 <sssssjin99@naver.com>
2022-01-15 13:15:32 +0900

    # 167 Solve: Unordered_map

--- Sujin/Ch22_Sorting/973.cpp
+++ Sujin/Ch22_Sorting/973.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
 #include <cmath>
+#include <queue>
+#include <vector>
 using namespace std;
 
-int main(){
-    cout<<hypot(4, 6)<<endl;
-    return 0;
-}
+class Solution {
+public:
+    struct cmp{
+        bool operator()(vector<int>&a, vector<int>&b){
+            double res1 = hypot(a[0], a[1]);
+            double res2 = hypot(b[0], b[1]);
+            
+            return res1 > res2;
+        }
+    };
+    
+    vector<vector<int> > kClosest(vector<vector<int> >& points, int k) {
+        priority_queue<vector<int>, vector<vector<int> >, cmp> pq;
+        
+        for(int i=0; i<points.size(); i++){
+            pq.push(points[i]);
+        }
+        
+        vector<vector<int> > res;
+        for(int i=0; i<k; i++){
+            res.push_back(pq.top());
+            pq.pop();
+        }
+        
+        return res;
+    }
+};
