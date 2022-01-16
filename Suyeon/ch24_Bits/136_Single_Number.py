082c433c79238017cf8044f2da50ab07bf2ad84c
suy2on <tnwjdrms@naver.com>
2022-01-16 16:49:07 +0900

    #  136 solve : xor 연산

--- Suyeon/ch24_Bits/136_Single_Number.py
+++ Suyeon/ch24_Bits/136_Single_Number.py
@@ -10,3 +10,10 @@ class Solution:
         for num in nums:
             if pair[num] == 1:
                 return num
+
+class Solution:
+    def singleNumber(self, nums) -> int:
+        for i in range(1,len(nums)):
+            nums[0] ^= nums[i] # a xor a = 0 , 0 xor a = a
+
+        return nums[0]
