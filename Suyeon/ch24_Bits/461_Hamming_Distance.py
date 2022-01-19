17f13c73ff7616e62474a7fd744f11729c49e627
suy2on <tnwjdrms@naver.com>
2022-01-19 15:19:19 +0900

    # 461 solve : count, xor

--- Suyeon/ch24_Bits/461_Hamming_Distance.py
+++ Suyeon/ch24_Bits/461_Hamming_Distance.py
@@ -2,3 +2,10 @@ class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
 
         return sum(map(int,(bin(x^y)[2:])))
+
+
+    
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+
+        return bin(x^y).count("1")
