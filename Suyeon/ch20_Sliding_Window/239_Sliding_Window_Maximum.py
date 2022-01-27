42ad0a9dcf7b29e7b098983892d6bca4a138a327
suy2on <tnwjdrms@naver.com>
2022-01-27 15:36:46 +0900

    # 239 solve : deque

--- Suyeon/ch20_Sliding_Window/239_Sliding_Window_Maximum.py
+++ Suyeon/ch20_Sliding_Window/239_Sliding_Window_Maximum.py
@@ -21,18 +21,48 @@ class Solution:
         # 초기값 잡기
         result.append(findMax(0))
 
-        for i in range(1, len(nums)-k+1):
+        for i in range(1, len(nums) - k + 1):
             if max_idx < i:
-                max_val = findMax(i)
+                if max_val > nums[i + k - 1]:
+                    max_val = findMax(i)
+                else:
+                    max_idx = i + k - 1
+                    max_val = nums[i + k - 1]
             else:
-                if nums[i] <= nums[i+k-1]:
-                    max_idx = i+k-1
-                    max_val = nums[i+k-1]
+                if max_val <= nums[i + k - 1]:
+                    max_idx = i + k - 1
+                    max_val = nums[i + k - 1]
+
             result.append(max_val)
 
         return result
 
 
+class Solution:
+    def maxSlidingWindow(self, nums, k):
+        result = []
+
+        for i in range(1, len(nums) - k + 1):
+            result.append(max(nums[i:i+3]))
+
+
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        from collections import deque
+        q = deque() # stores *indices*
+        res = []
+        for i, cur in enumerate(nums):
+            while q and nums[q[-1]] <= cur:
+                q.pop()
+            q.append(i)
+            # remove first element if it's outside the window
+            if q[0] == i - k:
+                q.popleft()
+            # if window has k elements add to results (first k-1 windows have < k elements because we start from empty window and add 1 element each iteration)
+            if i >= k - 1:
+                res.append(nums[q[0]])
+        return res
 
 sol = Solution()
 print(sol.maxSlidingWindow(nums = [1,3,-1,-3,5,3,6,7], k = 3))
