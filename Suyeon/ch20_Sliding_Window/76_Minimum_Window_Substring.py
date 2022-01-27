d247cd8ec3c595e654d3d4457816bec0958192de
suy2on <tnwjdrms@naver.com>
2022-01-27 22:55:56 +0900

    # 76 solve : two points and sliding window

--- Suyeon/ch20_Sliding_Window/76_Minimum_Window_Substring.py
+++ Suyeon/ch20_Sliding_Window/76_Minimum_Window_Substring.py
@@ -9,15 +9,15 @@ class Solution:
 
         result = ""
 
-        def check(substr):
+        def check(substr): # substring 판단
             for key in freqs.keys():
                 if substr[key] < freqs[key]:
                     return False
             return True
 
 
-        while right < len(s) :
-            print(s[left:right + 1])
+        while right < len(s) : # 가장왼쪽기준으로 substring찾기
+
             substr[s[right]] += 1
             if not check(substr):
                 right += 1
@@ -26,27 +26,28 @@ class Solution:
                 break
 
         while right < len(s)-1 :
-            print(s[left:right+1])
-            if check(substr):
-                result = s[left:right + 1]
+            if check(substr): # substring 맞으면 왼쪽 쭉 땡기기
+                result = s[left:right + 1] # 매번 최신 window 갱신
                 substr[s[left]] -= 1
                 left += 1
-            else:
+            else: # substring깨지면 그 윈도우 크기로 슬라이딩 하면서 다음 구간 찾기
                 substr[s[left]] -= 1
                 left += 1
                 right += 1
                 substr[s[right]] += 1
 
-        if check(substr):
-            while left <= right:
-                print(s[left:right + 1])
+        # right가 끝에 도달했을 때
+        if check(substr): # substring 있다면  pass
+            while left <= right: # 마지막으로 왼쪽 쭉 땡기기
+
                 if check(substr):
-                    print("hello")
+
                     substr[s[left]] -= 1
                     left += 1
                 else:
-                    result = s[left-1 :right + 1]
                     break
+            result = s[left - 1:right + 1] # substring깨진 직후니까 바로 전값으로 반환
+
 
         return result
 
