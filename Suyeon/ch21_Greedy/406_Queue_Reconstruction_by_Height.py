1bde798493fd4bb95a05b12f449186d128f33e48
suy2on <tnwjdrms@naver.com>
2022-02-02 20:52:57 +0900

    # feat : 406 최적화

--- Suyeon/ch21_Greedy/406_Queue_Reconstruction_by_Height.py
+++ Suyeon/ch21_Greedy/406_Queue_Reconstruction_by_Height.py
@@ -1,34 +1,37 @@
 import collections
 
-# class Solution:
-#     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
-#         people.sort(key= lambda x : (-x[0],x[1])) # 키에 대해서는 내림차순 앞에 있는 사람수에 대해서는 내림차순
-#         result = collections.deque()
-#         stack = []
-#
-#         for person in people:
-#             count = person[1]
-#
-#             # 앞에서 부터 pop
-#             while count !=0 and len(result) != 0:
-#                 stack.append(result.popleft())
-#                 count -= 1
-#
-#             # 적당한 자리에 넣어주기
-#             result.appendleft(person)
-#
-#             # 앞에 pop한거 다시넣어주기
-#             while len(stack) != 0:
-#                 result.appendleft(stack.pop())
-#
-#
-#         return result
+class Solution:
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        people.sort(key= lambda x : (-x[0],x[1])) # 키에 대해서는 내림차순 앞에 있는 사람수에 대해서는 내림차순
+        result = collections.deque()
+        stack = []
+
+        for person in people:
+            count = person[1]
+
+            # 앞에서 부터 pop
+            while count !=0 and len(result) != 0:
+                stack.append(result.popleft())
+                count -= 1
+
+            # 적당한 자리에 넣어주기
+            result.appendleft(person)
+
+            # 앞에 pop한거 다시넣어주기
+            while len(stack) != 0:
+                result.appendleft(stack.pop())
+
+
+        return result
 
 
 
-result = []
+class Solution:
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        people.sort(key=lambda x: (-x[0], x[1]))  # 키에 대해서는 내림차순 앞에 있는 사람수에 대해서는 내림차순
+        result = []
 
+        for person in people:
+            result.insert(person[1], person)
 
-result.insert(0,5)
-result.insert(3,2)
-print(result)
+        return result
