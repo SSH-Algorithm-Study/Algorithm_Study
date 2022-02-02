826dd187043152159e26df2fc1575623134ff222
suy2on <tnwjdrms@naver.com>
2022-02-02 20:24:39 +0900

    # feat  : 406 주석추가

--- Suyeon/ch21_Greedy/406_Queue_Reconstruction_by_Height.py
+++ Suyeon/ch21_Greedy/406_Queue_Reconstruction_by_Height.py
@@ -1,23 +1,34 @@
 import collections
 
-class Solution:
-    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
-        people.sort(key= lambda x : (-x[0],x[1]))
-        result = collections.deque()
-        stack = []
+# class Solution:
+#     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+#         people.sort(key= lambda x : (-x[0],x[1])) # 키에 대해서는 내림차순 앞에 있는 사람수에 대해서는 내림차순
+#         result = collections.deque()
+#         stack = []
+#
+#         for person in people:
+#             count = person[1]
+#
+#             # 앞에서 부터 pop
+#             while count !=0 and len(result) != 0:
+#                 stack.append(result.popleft())
+#                 count -= 1
+#
+#             # 적당한 자리에 넣어주기
+#             result.appendleft(person)
+#
+#             # 앞에 pop한거 다시넣어주기
+#             while len(stack) != 0:
+#                 result.appendleft(stack.pop())
+#
+#
+#         return result
 
-        for person in people:
-            count = person[1]
-            while count !=0 and len(result) != 0:
-                stack.append(result.popleft())
-                count -= 1
-
-            result.appendleft(person)
-
-            while len(stack) != 0:
-                result.appendleft(stack.pop())
 
 
-        return result
+result = []
 
 
+result.insert(0,5)
+result.insert(3,2)
+print(result)
