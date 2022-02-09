5bd594ed34dc1f9d153dc3681275c176b1e90293
suy2on <tnwjdrms@naver.com>
2022-02-09 15:33:12 +0900

    # 출력문추가

--- Suyeon/ch21_Greedy/621_Task_Scheduler.py
+++ Suyeon/ch21_Greedy/621_Task_Scheduler.py
@@ -3,7 +3,7 @@ import collections
 
 
 class Solution:
-    def leastInterval(self, tasks: List[str], n: int) -> int:
+    def leastInterval(self, tasks, n: int) -> int:
         pq = []  # 현재 가능한 일
         freqs = dict(collections.Counter(tasks))
         waitQ = collections.deque()  # 대기큐
@@ -14,20 +14,19 @@ class Solution:
 
         heapq.heapify(pq)  # 우선순위큐에 한번에 넣기
 
-        for _ in range(n + 1):  # 처음에 간격만큼 공백넣어주기
-            waitQ.append('#')
 
         while sum(freqs.values()) != 0:
+            print(waitQ)
             if waitQ:
                 task = waitQ.popleft()
 
                 if (task != "#"):
                     heapq.heappush(pq, (-freqs[task], task))
-
+            print(pq)
             if pq:  # 할일 있음
                 task = heapq.heappop(pq)
                 freqs[task[1]] -= 1
-
+                print(task[1])
                 if freqs[task[1]] != 0:  # 남은 일이라면 waitQ에 넣기
                     while len(waitQ) < n:  # 앞에 최소 n개의 일이 있도록
                         waitQ.append('#')
@@ -35,10 +34,11 @@ class Solution:
 
             else:  # 할일 없음
                 waitQ.append('#')
+                print("#")
 
             time += 1
-
+            print("=======================================")
         return time
 
 sol = Solution()
-print(sol.leastInterval(["A","A","A","A","A","A","B","C","D","E","F","G"],2))
+print(sol.leastInterval(["A","A","A","A","A","B","C","D","E","F","G"],2))
