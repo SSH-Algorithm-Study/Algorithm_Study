a9b7a1fee5f71a75d1714d034eeabc7afc0478bb
suy2on <tnwjdrms@naver.com>
2022-02-10 15:11:55 +0900

    # 134 :  주석추가

--- Suyeon/ch21_Greedy/134_Gas_Station.py
+++ Suyeon/ch21_Greedy/134_Gas_Station.py
@@ -1,7 +1,7 @@
 class Solution:
     def canCompleteCircuit(self, gas , cost) -> int:
         size = len(gas)
-        sub_list = [ gas[i] - cost[i] for i in range(size)]
+        sub_list = [ gas[i] - cost[i] for i in range(size)] # 차액계산
 
         # 예외처리
         if sum(sub_list) < 0 :
@@ -14,11 +14,11 @@ class Solution:
         head, tail = size-1, size-1
         total = sub_list[head]
 
-        while tail - head != size-1:
-            if total >= 0:
+        while tail - head != size-1: # 사이즈가 다다르면 끝
+            if total >= 0: # 차액이 음수가 아니면 : 유지
                 tail += 1
                 total += sub_list[tail % size]
-            else:
+            else: # 차액이 음수이면 : 시작점 옮기기
                 head -= 1
                 total += sub_list[head]
 
