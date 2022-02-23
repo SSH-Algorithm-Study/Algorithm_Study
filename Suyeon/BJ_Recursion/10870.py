a7fe2ce074dc93e8258e8a2999972489c7ba16e4
suy2on <tnwjdrms@naver.com>
2022-02-24 02:15:16 +0900

    # 10870 solve : recursion

---
+++ Suyeon/BJ_Recursion/10870.py
@@ -0,0 +1,10 @@
+def fibo_func(n):
+    if n < 2:
+        return n
+    if fibo[n]:
+        return fibo[n]
+    fibo[n] = fibo_func(n-1) + fibo_func(n-2)
+    return fibo[n]
+n = eval(input())
+fibo = [None] * (n+1)
+print(fibo_func(n))
