d25081966c7e87b43a16a7cc1e295a45fb2c46e3
suy2on <tnwjdrms@naver.com>
2022-02-24 18:14:39 +0900

    # 11729 solve : recursion

---
+++ Suyeon/BJ_Recursion/11729.py
@@ -0,0 +1,21 @@
+global count
+count = 0
+
+msg = []
+
+def hanoi(num,start,mid,finish):
+    global count
+    if num == 1:
+        msg.append("%d %d" %(start,finish))
+        count += 1
+    else:
+        hanoi(num-1,start,finish,mid)
+        msg.append("%d %d" %(start,finish))
+        count += 1
+        hanoi(num-1,mid,start,finish)
+
+n = eval(input())
+hanoi(n,1,2,3)
+print(count)
+for m in msg:
+    print(m)
