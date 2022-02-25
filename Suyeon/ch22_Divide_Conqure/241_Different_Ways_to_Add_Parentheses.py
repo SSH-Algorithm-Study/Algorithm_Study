5a2c873f2999acc76597e399510fa7ddb7920935
suy2on <tnwjdrms@naver.com>
2022-02-25 17:36:14 +0900

    # 241 수정: recursion

--- Suyeon/ch22_Divide_Conqure/241_Different_Ways_to_Add_Parentheses.py
+++ Suyeon/ch22_Divide_Conqure/241_Different_Ways_to_Add_Parentheses.py
@@ -1,43 +1,4 @@
 class Solution:
-    expression = ""
-    size = 0
-    answer = []
-
-    def calculate(self, a, opt, b):
-        print(a,opt,b)
-        if self.expression[opt] == "+":
-            return a+b
-        elif self.expression[opt] == "-":
-            return a-b
-        else:
-            return a*b
-
-    def partiton(self, head,tail):
-        global expression
-
-        if head == tail:
-            return int(self.expression[head])
-        p = head
-        return self.calculate(self.partiton(head, p),p+1, self.partiton(p+2,tail))
-
-    def merge(self,head,tail):
-        p = head
-        while p < self.size-2:
-            print(p)
-            self.answer.append(self.calculate(self.partiton(head,p), p+1, self.partiton(p+2,tail)))
-            p += 2
-
-
-    def diffWaysToCompute(self, expression: str):
-        self.expression = expression
-        self.size = len(expression)
-        self.merge(0,self.size-1)
-
-        return self.answer
-
-
-
-class Solution:
     def calculate(self,left,right,opt):
         result = []
         for l in left:
