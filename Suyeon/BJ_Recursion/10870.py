8a9153467d20aee8056503d5da0e4abc4c1c3b61
suy2on <tnwjdrms@naver.com>
2022-02-25 18:47:37 +0900

    # feat : 복습

--- Suyeon/BJ_Recursion/10870.py
+++ Suyeon/BJ_Recursion/10870.py
@@ -1,13 +1,24 @@
-def fibo_func(n):
-    if n < 2:
-        return n
-
-    if fibo[n]:
-        return fibo[n]
-
-    fibo[n] = fibo_func(n-1) + fibo_func(n-2)
-    return fibo[n]
+# def fibo_func(n):
+#     if n < 2:
+#         return n
+#
+#     if fibo[n]:
+#         return fibo[n]
+#
+#     fibo[n] = fibo_func(n-1) + fibo_func(n-2)
+#     return fibo[n]
+#
+# n = eval(input())
+# fibo = [None] * (n+1)
+# print(fibo_func(n))
 
 n = eval(input())
-fibo = [None] * (n+1)
-print(fibo_func(n))
+fibo = [None] * (n + 1)
+
+for i in range(n + 1):
+    if i < 2:
+        fibo[i] = i
+    else:
+        fibo[i] = fibo[i - 1] + fibo[i - 2]
+
+print(fibo[n])
--- Suyeon/BJ_Recursion/11729.py
+++ Suyeon/BJ_Recursion/11729.py
@@ -4,17 +4,18 @@ msg = []
 def hanoi(num,start,mid,finish):
     global count
     if num == 1:
-        msg.append("%d %d" %(start,finish))
+        msg.append((start,finish)) # n이 하나면 경유 없이 바로 finish로 옮기기
         count += 1
     else:
-        hanoi(num-1,start,finish,mid)
-        msg.append("%d %d" %(start,finish))
+        hanoi(num-1,start,finish,mid) # n-1개 원판 2번째로 옮기기
+        msg.append((start,finish)) # n번째 원판 3번째로 옮기기
         count += 1
-        hanoi(num-1,mid,start,finish)
+        hanoi(num-1,mid,start,finish) # n-1개 원판 3번째로 옮기기
 
 n = eval(input())
 hanoi(n,1,2,3)
 
+# 출력
 print(count)
 for m in msg:
-    print(m)
+    print(*m) # 튜플 언팩
