a0fd8f5b347ff52b43f97defb4f31f42b075e0d8
suy2on <tnwjdrms@naver.com>
2022-05-12 02:05:21 +0900

    # 11053, 9251 solve : DP

---
+++ Suyeon/BJ_DP/11053.py
@@ -0,0 +1,13 @@
+N = int(input())
+nums = list(map(int,input().split()))
+
+LIS = [1] * N
+
+for i in range(1,N):
+    j=0
+    while j < i:
+        if nums[j] < nums[i]:
+            LIS[i] = max(LIS[i], LIS[j]+1)
+        j += 1
+print(LIS)
+print(LIS[N-1])
